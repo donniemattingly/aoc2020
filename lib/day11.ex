bbf8ca29351120719681da2639bbfcad8ea951e3
Donnie Mattingly <donniewmattingly@gmail.com>
2020-12-11 18:20:26 -0500

    day 11pt2

--- lib/day11.ex
+++ lib/day11.ex
@@ -49,6 +49,14 @@ defmodule Day11 do
     """
   end
 
+  def sample_input4 do
+    """
+    .............
+    .L.L.#.#.#.#.
+    .............
+    """
+  end
+
   def sample do
     sample_input()
     |> parse_input1
@@ -139,7 +147,7 @@ defmodule Day11 do
 
     deltas
     |> Enum.map(&get_first_visible_seat_in_direction(p, &1, map))
-    |> Enum.filter(& &1 != p)
+    |> Enum.filter(&(&1 != p))
     |> Enum.filter(& &1)
   end
 
@@ -208,15 +216,16 @@ defmodule Day11 do
   end
 
   def tick2(map) do
-    new = Map.keys(map)
-    |> Enum.map(fn p -> update_tile2(p, Map.get(map, p), map) end)
-    |> Map.new()
+    new =
+      Map.keys(map)
+      |> Enum.map(fn p -> update_tile2(p, Map.get(map, p), map) end)
+      |> Map.new()
 
     p = {x, y} = {0, 0}
     seat = get_first_visible_seat_in_direction(p, {1, 0}, map)
     IO.inspect(seat, label: "visible seat going {0, 1} for {#{x}, #{y}}")
 
-    v = visible_seats({0, 0}, map) |> IO.inspect
+    v = visible_seats({0, 0}, map) |> IO.inspect()
     map_with_highlights(map, {1, 0}, v) |> render
 
     new
@@ -234,7 +243,6 @@ defmodule Day11 do
   end
 
   def do_ticks2(map, old_map, count \\ 0, limit \\ 4) do
-
     if is_stable(map, old_map) || count > limit do
       IO.puts("Stablized")
       map
@@ -247,14 +255,6 @@ defmodule Day11 do
   def solve(input) do
     input
     |> do_ticks(nil)
-    |> Map.to_list()
-    |> Enum.filter(fn {k, v} -> v == :full end)
-    |> Enum.count()
-  end
-
-  def solve2(input) do
-    input
-    |> do_ticks2(nil)
     |> Map.to_list()
     |> Enum.filter(fn {k, v} -> v == :full end)
     |> Enum.count()
@@ -288,4 +288,70 @@ defmodule Day11 do
 
     IO.puts("    ")
   end
+
+  def seat_visible_in_direction(map, {x, y}, {dx, dy}) do
+    Stream.unfold({x + dx, y + dy}, fn p = {x, y} -> {p, {x + dx, y + dy}} end)
+    |> Stream.map(&{&1, Map.get(map, &1)})
+    |> Stream.drop_while(fn {_, type} -> type == :floor end)
+    |> Stream.take(1)
+    |> Enum.to_list()
+    |> hd
+  end
+
+  def get_num_occupied_seats_visible(map, point) do
+    [{-1, 0}, {-1, -1}, {-1, 1}, {0, -1}, {0, 1}, {1, 0}, {1, -1}, {1, 1}]
+    |> Enum.map(fn direction -> seat_visible_in_direction(map, point, direction) end)
+    |> Enum.count(fn {_, state} -> state == :full end)
+  end
+
+  def type_of_next_tile(type, num_occupied_seats_visible) do
+    case type do
+      :full when num_occupied_seats_visible > 4 -> :empty
+      :empty when num_occupied_seats_visible == 0 -> :full
+      _ -> type
+    end
+  end
+
+  def next_tile(map, point) do
+    visible = get_num_occupied_seats_visible(map, point)
+    next_type = map |> Map.get(point) |> type_of_next_tile(visible)
+    {point, next_type}
+  end
+
+  def evolve(map) do
+    Stream.unfold(map, fn m ->
+      new_map =
+        Map.keys(m)
+        |> Stream.map(fn point -> next_tile(m, point) end)
+        |> Map.new()
+      {m, new_map}
+    end)
+  end
+
+  def get_stable_evolution(map) do
+    evolve(map)
+    |> Stream.chunk_every(2, 1)
+    |> Stream.drop_while(fn [a, b] -> a != b end)
+    |> Stream.take(1)
+    |> Enum.to_list
+    |> hd
+    |> hd
+  end
+
+  def count_full_seats(map) do
+    Map.values(map)
+    |> Enum.count(fn x -> x == :full end)
+  end
+
+  def get_n_ticks(map, num_ticks) do
+    evolve(map)
+    |> Stream.take(num_ticks)
+    |> Stream.run()
+  end
+
+  def solve2(input) do
+    get_stable_evolution(input)
+    |> count_full_seats()
+  end
+
 end
